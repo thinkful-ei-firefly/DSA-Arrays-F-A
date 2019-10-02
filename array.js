152e2980e4b7ce83d1a819fc02e4233e73fa9a6e
Fabian Lema <fabianlemac@gmail.com>
2019-10-02 11:03:27 -0500

    push method

--- array.js
+++ array.js
@@ -1,4 +1,5 @@
-import memory from './memory'
+const mem = require('./memory');
+const memory = new mem();
 
 class Array {
     constructor() {
@@ -6,4 +7,48 @@ class Array {
         this._capacity = 0;
         this.ptr = memory.allocate(this.length);
     }
+
+    push(value){
+      if (this.length >= this._capacity){
+        this._resize((this.length + 1) * Array.SIZE_RATIO)
+      }
+      memory.set(this.ptr + this.length, value);
+      this.length++;
+    }
+
+    _resize(size) {
+      const oldPtr = this.ptr;
+      this.ptr = memory.allocate(size);
+      if (this.ptr === null){
+        throw new Error('Out of memory');
+      }
+      memory.copy(this.ptr, oldPtr, this.length)
+      memory.free(oldPtr);
+      this._capacity = size;
+    }
 }
+
+Array.SIZE_RATIO = 3;
+
+
+
+function main(){
+  Array.SIZE_RATIO = 3;
+
+  let arr = new Array();
+
+  arr.push(3);
+  console.log(arr);
+  arr.push(5);
+  console.log(arr);
+  arr.push(15);
+  console.log(arr);
+  arr.push(19);
+  console.log(arr);
+  arr.push(45);
+  console.log(arr);
+  arr.push(10);
+  console.log(arr);
+}
+
+main();
