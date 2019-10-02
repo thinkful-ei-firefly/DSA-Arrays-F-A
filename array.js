8d4829bfada3124b05635505602b53bfd6238e0d
Ahmed <ahmed.max7586@gmail.com>
2019-10-02 11:25:42 -0400

    initalized

---
+++ array.js
@@ -0,0 +1,9 @@
+import memory from './memory'
+
+class Array {
+    constructor() {
+        this.length = 0;
+        this._capacity = 0;
+        this.ptr = memory.allocate(this.length);
+    }
+}
