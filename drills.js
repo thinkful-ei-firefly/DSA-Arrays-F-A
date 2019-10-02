8fe4fbdfd49dd525832693011e1b9b30fe0c5723
Ahmed <ahmed.max7586@gmail.com>
2019-10-02 14:38:36 -0400

    added maxsum.js

--- drills.js
+++ drills.js
@@ -23,4 +23,23 @@ function filter(array, number){
   return result;
 }
 
-console.log(filter([5,1,6,2,7,8], 5));
+// console.log(filter([5,1,6,2,7,8], 5));
+
+
+function maxSum(arr) {
+    let max = 0;
+     for (let i = 0 ; i < arr.length; i ++ ){
+         let sum = 0;
+        for (let j = i; j < arr.length; j++){
+            sum += arr[j]
+            // console.log(sum);
+            if (max < sum) {
+                max = sum
+            }
+        }
+     }
+     return max;
+}
+
+// console.log(maxSum([-4, 6, -3, 5, -2, 1]));     //12
+
